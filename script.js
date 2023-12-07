5c8867a1088ac7e309c8eb0339aef14a5525ee1c
Anastasiia <humenyuknastia.9616@gmail.com>
2023-12-07 23:13:46 +0100

    update html/css

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 });
 
+function checkAnswer(selectOption){
+    const correctAnswer = 'c';
+     // Get all elements with the class 'answer' (answer options)
+    const options = document.querySelector(".answer");
+
+    options.forEach(option => {
+        const label = document.querySelector(`label[for ="${option.id}"]`)
+        if(option === selectOption){
+            if(option.classList.contains("answer-correct")){
+                label.style.color = "green";
+            }else{
+               label.style.color ="red";
+            }
+        }
+    });
+}
+
+// Додаємо обробники подій до варіантів відповідей
+
+const answerOptions = document.querySelectorAll(".answer");
+answerOptions.forEach(option =>{
+    // Add a 'click' event listener to each answer option
+    option.addEventListener("click", function(){
+        checkAnswer(option);
+    })
+})
+
+
